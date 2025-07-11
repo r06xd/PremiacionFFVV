df5a7ce4446d9c9336ac555bdb73c5603b424231
r06xd <r06xd@hotmail.es>
2025-07-11 11:04:31 -0500

    Modificaciones 2

--- src/main/java/com/uisrael/Repository/KPIRepository.java
+++ src/main/java/com/uisrael/Repository/KPIRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.uisrael.Entity.Kpi;
 
 public interface KPIRepository extends JpaRepository<Kpi, Long> {
+	
+	
     List<Kpi> findByEmpresaId(Long empresaId);
 }
